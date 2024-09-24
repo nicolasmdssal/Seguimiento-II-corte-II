c01a1e31f8df332d196ebc12ecd95be5617838f8
nicolasmdssal <158488256+nicolasmdssal@users.noreply.github.com>
2024-09-24 11:31:36 -0500

    mensaje

---
+++ persistenciaArchivos/src/main/java/org/example/domain/Persona.java
@@ -0,0 +1,53 @@
+package org.example.domain;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String nombre;
+    private String contraseña;
+
+    // Constructores, getters y setters
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, String contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
